6044c04f3c0e1d7acdc423517c7f94505ccd08f6
Corina Toweh <162394856+ctoweh@users.noreply.github.com>
2024-04-16 02:49:22 +0100

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/") 
 def hello():
-  return "I am almost a Devops Engineer!"
+  return "Hi, I am almost a Devops Engineer!"
 
 if __name__ == "__main__":
    app.run(host='0.0.0.0', port=5000)

d78606746cf792eba5da410bc8ffff7ded9d65ec
Corina Toweh <162394856+ctoweh@users.noreply.github.com>
2024-04-16 23:59:36 +0100

    app

--- app.py
+++ app.py
@@ -8,4 +8,4 @@ def hello():
 
 if __name__ == "__main__":
     # Run the Flask app on host 0.0.0.0 and port 5000
-    app.run(host='0.0.0.0', port=5000)
+    app.run(use_reloader=True, host='0.0.0.0', port=5000)

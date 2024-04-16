8791a297a06c47e4f37728f4a3c6d006225a3ef9
Corina Toweh <162394856+ctoweh@users.noreply.github.com>
2024-04-16 23:27:49 +0100

    app

--- app.py
+++ app.py
@@ -1,1 +1,9 @@
+from flask import Flask
+app = Flask(__name__)
 
+@app.route("/") 
+def hello():
+  return "I am almost a Devops Engineer! Corina&Chiamaka"
+
+if __name__ == "__main__":
+   app.run(host='0.0.0.0', port=5000)

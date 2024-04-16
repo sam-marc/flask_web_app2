1ec664e95cf795d393d47401b7e1810f495535a6
Corina Toweh <162394856+ctoweh@users.noreply.github.com>
2024-04-16 23:48:15 +0100

    app

--- app.py
+++ app.py
@@ -2,10 +2,10 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/") 
+@app.route("/")
 def hello():
     return "I am almost a DevOps Engineer! Corina&Chiamaka"
 
 if __name__ == "__main__":
-    # Listen on all network interfaces
+    # Run the Flask app on host 0.0.0.0 and port 5000
     app.run(host='0.0.0.0', port=5000)
